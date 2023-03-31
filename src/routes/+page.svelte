90bc0de1c5fc35a1f408dedf3e4428d3187e4da7
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-03-31 14:22:13 +0200

    feat: :sparkles: added implementation for testemonial card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,31 @@
-<script lang="ts">
-	import TestimonialCard from '$lib/challenge-1/TestimonialCard.svelte';
-</script>
 
 <div class="flex min-h-screen items-center justify-center bg-sky-blue">
-	<TestimonialCard />
+	<div class="bg-cover rounded-lg space-y-4 shadow-lg" style="background-image: url('pascal-bullan-InwZeTM1DL0-unsplash.jpg');">
+		<div class="bg-white p-2 rounded-lg mt-10 mr-10 ml-10 mb-10">
+			<div>
+				<img src="qr-github-com.svg" alt="">
+			</div>
+		</div>
+		<div class="bg-white rounded-b-lg">
+			<h2 class="text-center m-5" style="color: #24657A;">Welcome to Frontend Fridays</h2>
+			<p class="text-center font-light">
+				"Frontend Fridays are a lot 
+			</p>
+			<p class="text-center font-light">
+				of fun and it's good for impatient 
+			</p>
+			<p class="text-center font-light">
+				people, cause you don't have to 
+			</p>
+			<p class="text-center font-light mb-5">
+				wait to see your errors."
+			</p>
+			<p class="text-center font-light">
+				Stefanie, ISA DEV2
+			</p>
+			<div class="flex items-center justify-center">
+				<button class="bg-dark-teal p-3 rounded-lg mb-5 mt-5 justify-center" style="color: white;">Learn More</button>
+			</div>
+		</div>
+	</div>
 </div>
-
-<style>
-</style>
