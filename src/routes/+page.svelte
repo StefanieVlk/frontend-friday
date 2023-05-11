50335b615591771da9ef6c4aafebb9a090114ad5
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-05-11 15:44:24 +0200

    refactor: :recycle: improved breaking lines

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,18 +9,12 @@
 		<div class="bg-white rounded-b-lg">
 			<h2 class="text-center m-5" style="color: #24657A;">Welcome to Frontend Fridays</h2>
 			<p class="text-center font-light">
-				"Frontend Fridays are a lot 
-			</p>
-			<p class="text-center font-light">
-				of fun and it's good for impatient 
-			</p>
-			<p class="text-center font-light">
-				people, cause you don't have to 
-			</p>
-			<p class="text-center font-light mb-5">
+				"Frontend Fridays are a lot<br>
+				of fun and it's good for impatient<br>
+				people, cause you don't have to<br>
 				wait to see your errors."
 			</p>
-			<p class="text-center font-light">
+			<p class="text-center font-light mt-5">
 				Stefanie, ISA DEV2
 			</p>
 			<div class="flex items-center justify-center">
