74aeeaff0931062740fb3e2642236a64ada8ed2f
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-05-11 17:48:28 +0200

    feat: :sparkles: added basic implementation of rating component

---
+++ src/routes/rating/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import star from './star-svgrepo-com.svg';
+</script>
+
+<div class="bg-[#141519] w-screen h-screen grid place-items-center">
+    <div class="bg-[#1f262e] rounded-2xl w-1/4 p-5">
+        <div class="bg-[#262f38] w-12 h-12 rounded-full grid place-items-center mb-5">
+            <img src={star} alt="" width="35%">
+        </div>
+        <h1 class="text-white font-bold mb-3 text-xl">How did we do?</h1>
+        <p class="text-white/50 font-light mb-3">
+            Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!
+        </p>
+        <div class="mb-5">
+            <button class="mr-4 h-12 w-12 bg-[#262f38] text-white/50 font-light rounded-full">1</button>
+            <button class="mr-4 h-12 w-12 bg-[#262f38] text-white/50 font-light rounded-full">2</button>
+            <button class="mr-4 h-12 w-12 bg-[#262f38] text-white/50 font-light rounded-full">3</button>
+            <button class="mr-4 h-12 w-12 bg-[#262f38] text-white/50 font-light rounded-full">4</button>
+            <button class="h-12 w-12 bg-[#262f38] text-white/50 font-light rounded-full">5</button>
+        </div>
+        <div class="grid place-items-center">
+            <button class="text-white bg-[#fc7613] w-10/12 h-12 rounded-full">SUBMIT</button>
+        </div>
+        
+        
+    </div>
+</div>
